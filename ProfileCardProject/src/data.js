896f433d942f84fbe20ef4bfc0634f8e92a49b9e
Zine EL Abidine DKIR Elmoubtakir <95342581+ZineelabidineDkirDev@users.noreply.github.com>
2024-01-03 14:04:43 +0000

    Add data

---
+++ ProfileCardProject/src/data.js
@@ -0,0 +1,131 @@
+const userData = [ 
+
+    { 
+  
+      id: 1, 
+  
+      name: "ZINE EL ABIDINE DKIR", 
+  
+      description: "Full stack .Net/React developer", 
+  
+      imageUrl: "https://www.iconfinder.com/icons/4883396/avatar_boy_male_man_people_profile_icon", 
+  
+      age: "27 ans", 
+  
+      mobilite: "national", 
+  
+      disponibilité: "immédiate", 
+  
+      location: "Rabat, Maroc", 
+  
+      interests: ["Sport", "Voyage"] 
+  
+    },
+    { 
+  
+        id: 2, 
+    
+        name: "WAFAA HACHEM", 
+    
+        description: "Full stack .Net/React developer", 
+    
+        imageUrl: "https://www.iconfinder.com/icons/4883393/avatar_female_girl_people_profile_woman_icon", 
+    
+        age: "25 ans", 
+    
+        mobilite: "national", 
+    
+        disponibilité: "immédiate", 
+    
+        location: "Salé, Maroc", 
+    
+        interests: ["Sport", "Voyage","Lecture"] 
+    
+      },
+      {
+ 
+        id: 3,
+     
+        name: "Younes Biloul",
+     
+        description: "Full stack .Net/React developer",
+     
+        imageUrl: "https://as2.ftcdn.net/v2/jpg/03/64/21/11/1000_F_364211147_1qgLVxv1Tcq0Ohz3FawUfrtONzz8nq3e.jpg",
+     
+        age: "23 ans",
+     
+        mobilite: "marocaine",
+     
+        disponibilité: "immédiate",
+     
+        location: "kenitra, Maroc",
+     
+        interests: ["Musique", "Voyage", "Lecture"]
+     
+      },
+      ,
+      {
+ 
+        id: 4,
+     
+        name: "Abdennour Rhamri",
+ 
+        description: "Full stack .Net/React developer",
+    
+        imageUrl: "https://as2.ftcdn.net/v2/jpg/03/64/21/11/1000_F_364211147_1qgLVxv1Tcq0Ohz3FawUfrtONzz8nq3e.jpg",
+    
+        age: "23 ans",
+    
+        mobilite: "national",
+    
+        disponibilité: "immédiate",
+    
+        location: "Tanger, Maroc",
+    
+        interests: ["Musique", "Voyage", "Lecture"]
+     
+      },
+      { 
+
+        id: 5, 
+        
+        name: "El mustapha EL MOJAHID", 
+        
+        description: "Full stack .Net/React developer", 
+        
+        imageUrl: "https://www.iconfinder.com/icons/2708417/avatar_boy_man_user_icon", 
+        
+        age: "23 ans", 
+        
+        mobilite: "national", 
+        
+        disponibilité: "immédiate", 
+        
+        location: "Rabat, Maroc", 
+        
+        interests: ["Equitation", "Voyage", "Lecture"] 
+        
+        },
+        {
+ 
+            id: 6,
+         
+            name: "Youssef Amjad",
+         
+            description: "Full stack .Net/React developer",
+         
+            imageUrl: "blob:https://teams.microsoft.com/2f7982d7-6940-4d2c-b239-a5d3dfd6a6e6",
+         
+            age: "21 ans",
+         
+            mobilite: "marocaine",
+         
+            disponibilité: "immédiate",
+         
+            location: "Sale, Maroc",
+         
+            interests: ["Musique", "Sport", "Calme"]
+         
+          },
+  
+  ];
