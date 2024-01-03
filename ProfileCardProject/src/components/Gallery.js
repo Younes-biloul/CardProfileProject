cf35a660566d17c2c22a002d3385a2e6f096171b
Zine EL Abidine DKIR Elmoubtakir <95342581+ZineelabidineDkirDev@users.noreply.github.com>
2024-01-03 14:32:59 +0000

    model react

---
+++ ProfileCardProject/src/components/Gallery.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Profiles from './Profiles'
+
+const Gallery = (props) => {
+  return (
+    <div>
+    <p>---Navbar</p>
+
+    <Profiles/>
+    
+    </div>
+  )
+}
+
+export default Gallery
---
+++ ProfileCardProject/src/components/Profiles.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Profiles = (props) => {
+  return (
+    <div>
+
+    <h2>---Profiles</h2>
+    
+    
+    </div>
+  )
+}
+
+export default Profiles
